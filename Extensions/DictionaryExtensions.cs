71f6322c476a0cb7c6ddc393719f29cada642eff
jrosenbergpc-dev <jrosenbergpc@gmail.com>
2024-12-02 16:57:38 -0500

    Latest update, refactored a few things to increase maintainablity, added dictionary extensions class, working on standarizing certain css styling for controls but allowing the user to add their own coloring styles to override the defaults.

---
+++ Extensions/DictionaryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Style_Forge_Blazor.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static Dictionary<TKey, TValue> MergeWith<TKey, TValue>(this Dictionary<TKey, TValue> defaults, Dictionary<TKey, TValue>? custom)
+        {
+            if (custom == null)
+            {
+                return new Dictionary<TKey, TValue>(defaults); // Ensure the original dictionary isn't modified
+            }
+
+            return defaults.Concat(custom).GroupBy(kvp => kvp.Key).ToDictionary(group => group.Key, group => group.Last().Value);
+        }
+    }
+}
