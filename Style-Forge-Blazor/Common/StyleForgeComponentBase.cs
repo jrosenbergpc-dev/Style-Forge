aa869f1b37082ffb7e8c4d878378ee48c796f809
jrosenbergpc-dev <jrosenbergpc@gmail.com>
2024-12-19 16:57:52 -0500

    Updated Base Component Class and added Button Control to project, need to organize namespacing and razor control names as there might be future conflicts with the current naming convention.

--- Style-Forge-Blazor/Common/StyleForgeComponentBase.cs
+++ Style-Forge-Blazor/Common/StyleForgeComponentBase.cs
@@ -4,14 +4,31 @@ namespace Style_Forge.Common
 {
 	public abstract class StyleForgeComponent : ComponentBase
 	{
-		/// <summary>
-		/// CSS Properties can be set here by adding a new CSSProperty<T> To the collection.
-		/// </summary>
-		[Parameter] public List<CSSPropertyBase> CSSProperties { get; set; } = new List<CSSPropertyBase>();
+        private static int _counter = -1;
+
+        [Parameter] public int ID { get; set; }
+        [Parameter] public string? Name { get; set; }
+        [Parameter] public string? Text { get; set; }
+        [Parameter] public bool Selected { get; set; }
+        [Parameter] public bool Disabled { get; set; }
+        [Parameter] public string? Value { get; set; }
+        [Parameter] public string? SelectedValue { get; set; }
+
+        /// <summary>
+        /// CSS Properties can be set here by adding a new CSSProperty<T> To the collection.
+        /// </summary>
+        [Parameter] public List<CSSPropertyBase> CSSProperties { get; set; } = new List<CSSPropertyBase>();
 
 		/// <summary>
 		/// OnClick Event Callback 
 		/// </summary>
 		[Parameter] public EventCallback<dynamic> OnClicked { get; set; }
+        [Parameter] public EventCallback<dynamic> OnDoubleClicked { get; set; }
+        [Parameter] public EventCallback<dynamic> OnSelectionChanged { get; set; }
+
+        public StyleForgeComponent()
+		{
+            ID = Interlocked.Increment(ref _counter);
+        }
 	}
 }
